2181d23ce0e78a02f8269c0f428f3a78e9522987
davidcoats <david@getsafetycone.com>
2023-01-30 08:26:23 -0800

    Initial commit.

---
+++ source/R5T.S0062/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.S0062
+{
+	/// <summary>
+	/// Content paths script.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.S0062/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.S0062
+Content paths script.
